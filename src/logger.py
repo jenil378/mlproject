355f1eac09ca446696657ddc529b6db2b7ef9a46
jenil378 <vekariyajenil23@gmail.com>
2025-01-19 13:12:15 +0530

    Data Transformation done

--- src/logger.py
+++ src/logger.py
@@ -1,25 +1,51 @@
+# import logging
+# import os
+# from datetime import datetime
+
+# # Generate a unique log file name with a timestamp
+# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# # Define the logs directory relative to the script location
+# logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "src", "logs")
+
+# # Create the logs directory if it doesn't exist
+# os.makedirs(logs_dir, exist_ok=True)
+
+# # Full path for the log file
+# LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# # Configure logging
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     level=logging.INFO
+# )
+
+# # Optional: Add log rotation if required
+# # from logging.handlers import RotatingFileHandler
+# # handler = RotatingFileHandler(LOG_FILE_PATH, maxBytes=10*1024*1024, backupCount=5)
+# # logging.getLogger().addHandler(handler)
+
+# if __name__ == "__main__":
+#     logging.info(f"Log file created at: {LOG_FILE_PATH}")
+#     logging.info("Logging has started")
+
+
+
 import logging
 import os
 from datetime import datetime
 
-# Generate a unique log file name with timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Define the logs directory relative to the src folder
-logs_dir = os.path.join(os.getcwd(), "src", "logs")
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Create the logs directory if it doesn't exist
-os.makedirs(logs_dir, exist_ok=True)
-
-# Full path for the log file
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
-
-# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
-if __name__ == "__main__":
-    logging.info("Logging has started")
+
+)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,21 @@
 #for calling or loading the datset
 #for saving the model on the cloud
 #then directly linking/calling into the components
+#It has Some common Functionalities that can be used throughout the function
+import os
+import sys
+import dill
 
- 
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
